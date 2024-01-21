25fe3ae08a973baef1f7c9b2d4189e5a72215c79
Basak Kaya <basakkaya165@icloud.com>
2024-01-21 18:02:16 +0300

    This branch TabBarController and CategoryScreen has been created.

--- MusicApp/Screens/CategoryScreen/View/CategoryViewController.swift
+++ MusicApp/Screens/CategoryScreen/View/CategoryViewController.swift
@@ -6,18 +6,97 @@
 //
 
 import Foundation
+import Kingfisher
 import UIKit
 
 class CategoryViewController: UIViewController {
     
+    private let cellId = "cellId"
+    
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(CategoryCollectionCell.self, forCellWithReuseIdentifier: cellId)
+        return collectionView
+        
+    }()
+    
+    var dictionary = [String: Any]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let categoryService = CategoryService()
-        categoryService.fetchMusicData(url: "https://api.deezer.com/genre") { responseDict in
+        
+        view.addSubview(collectionView)
+        
+        collectionView.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.left.right.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
+        
+        let viewModel = CategoryViewModel()
+        viewModel.getCategoryData { responseDict in
             print(responseDict)
+            self.dictionary = responseDict
+            DispatchQueue.main.async {
+                self.collectionView.reloadData()
+            }
         } onError: { error in
             
         }
+        
+        
+    }
+}
 
+extension CategoryViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return (dictionary.values.first as? [Any])?.count ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        
+        if let cell = cell as? CategoryCollectionCell {
+            cell.backgroundColor = .cyan
+            if let imageUrl = (dictionary.values.first as? [[String : Any]])?[indexPath.row]["picture"] as? String {
+                let url = URL(string: imageUrl)
+                cell.imageView.kf.setImage(with: url)
+            }
+            if let name = (dictionary.values.first as? [[String : Any]])?[indexPath.row]["name"] as? String {
+                cell.labelView.text = name
+            }
+        }
+        return cell
+    }
+}
+
+extension CategoryViewController: UICollectionViewDelegate {
+    
+    //kategorilerin tıklanabilmesi için buraya bir func bulman gerek//
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
+}
+
+extension CategoryViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: (view.frame.width/2) - 2 , height: view.frame.width / 2)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 2
     }
 }
--- MusicApp/Screens/CategoryScreen/ViewModel/CategoryViewModel.swift
+++ MusicApp/Screens/CategoryScreen/ViewModel/CategoryViewModel.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+class CategoryViewModel {
+    
+    func getCategoryData(onSuccess: @escaping([String : Any]) -> Void, onError: @escaping(String) -> Void) {
+        let networkManager = NetworkManager()
+        networkManager.fetchData(url: "https://api.deezer.com/genre") { responseDict in
+        onSuccess(responseDict)
+            
+        } onError: { error in
+            onError(error)
+        }
+        
+      
+              
+    }
+    
+}
